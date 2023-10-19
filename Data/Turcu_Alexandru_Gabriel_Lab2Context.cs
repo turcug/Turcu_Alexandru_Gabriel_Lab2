b690f0bb03e782d7f5390ecd4ee5f2fc2f154b6a
Alexandru Gabriel <Alexandru Gabriel@DESKTOP-3QN92AP>
2023-10-19 19:15:30 +0300

    Add project files.

---
+++ Data/Turcu_Alexandru_Gabriel_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Turcu_Alexandru_Gabriel_Lab2.Models;
+
+namespace Turcu_Alexandru_Gabriel_Lab2.Data
+{
+    public class Turcu_Alexandru_Gabriel_Lab2Context : DbContext
+    {
+        public Turcu_Alexandru_Gabriel_Lab2Context (DbContextOptions<Turcu_Alexandru_Gabriel_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Turcu_Alexandru_Gabriel_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Turcu_Alexandru_Gabriel_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Turcu_Alexandru_Gabriel_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+
+    }
+}
